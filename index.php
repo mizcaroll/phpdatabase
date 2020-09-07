2eee5e7cc5ce5f031320356b7ebcdc6eb000a87f
mizcaroll <carolomukule@gmail.com>
2020-09-07 19:50:39 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'includes/dbh.inc.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link type="text/css" rel="stylesheet" href="styles.css">
+  <title>PHPdatabase</title>
+</head>
+<body>
+  $conn;
+
+</body>
+</html>
