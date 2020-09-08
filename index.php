4ce790973b80b9c932e8c40711ed1d55926494cf
mizcaroll <carolomukule@gmail.com>
2020-09-08 17:09:02 +0300

    Msqli queries in php

--- index.php
+++ index.php
@@ -11,7 +11,17 @@ include_once 'includes/dbh.inc.php';
   <title>PHPdatabase</title>
 </head>
 <body>
-  $conn;
+  <?php
+$sql = "SELECT * FROM users;";
+$result = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+if ($resultCheck > 0) {
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['user_uid'] . "<br>";
+  }
+}
+
+  ?>
 
 </body>
 </html>
