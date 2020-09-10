37aba3b0f53cbb331e5ac86247197a99fb7c4905
mizcaroll <carolomukule@gmail.com>
2020-09-10 16:37:30 +0300

    Advance error handling method 1

--- includes/signup2.inc.php
+++ includes/signup2.inc.php
@@ -1,26 +1,53 @@
 <?php
 //error handling script + uploading to the database
 
+//check if user has clicked the signup button
 if (isset($_POST['submit'])) {
+
+  //include the database connection
 include_once 'dbh.inc.php';
 
+//get the data from the signup form
 $first = mysqli_real_escape_string($conn, $_POST['first']);
 $last = mysqli_real_escape_string($conn, $_POST['last']);
 $email =mysqli_real_escape_string($conn, $_POST['email']);
 $uid = mysqli_real_escape_string($conn, $_POST['uid']);
 $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
 
+//check whether inputs are empty
 if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
   header("Location: ../index.php?signup=empty");
+  exit();
+} else {
 
+  //check if input characters are valid
+if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
+  header("Location: ../index.php?signup=char");
+  exit(); 
 } else {
+
+//check if email is valid
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  header("Location: ../index.php?signup=invalidemail");
+  header("Location: ../index.php?signup=email");
 } else {
-  echo "You have signed up successfully.";
+
+  //signup successfully
+  $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
+
+$stmt = mysqli_stmt_init($conn);
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+  echo "SQL error";
+} else {
+mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
+mysqli_stmt_execute($stmt);
+}
+  header("Location: ../index.php?signup=success");
+  exit();
+}
 }
 }
 }
 else {
   header("Location: ../index.php?signup=error");
+  exit();
 }
--- index.php
+++ index.php
@@ -11,6 +11,7 @@ include_once 'includes/dbh.inc.php';
   <title>PHPdatabase</title>
 </head>
 <body>
+ 
 <h2>Sign Up</h2>
 <form action="includes/signup2.inc.php" method="POST">
 <input type="text" name="first" placeholder="Firstname">
@@ -25,7 +26,26 @@ include_once 'includes/dbh.inc.php';
 <br>
 <button type="submit" name="submit">Sign up</button>
 </form>
+<?php
+$fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
+if (strpos($fullUrl, "signup=empty") == true) {
+  echo "<p class='error'>Please fill in all fields!</p>";
+  exit();
+}
+elseif (strpos($fullUrl, "signup=char") == true) {
+  echo "<p class='error'>Please use valid characters!</p>";
+  exit();
+}
+elseif (strpos($fullUrl, "signup=email") == true) {
+  echo "<p class='error'>Please use a valid e-mail address!</p>";
+  exit();
+}
+elseif (strpos($fullUrl, "signup=success") == true) {
+  echo "<p class='success'>You have been signed up successfully!</p>";
+  exit();
+}
+?>
 </body>
 </html>
 
