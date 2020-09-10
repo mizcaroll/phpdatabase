a04a3bedc7c9deb9deee3760b0a67d0e2200a1de
mizcaroll <carolomukule@gmail.com>
2020-09-10 19:05:03 +0300

    Advanced error handling METHOD 2

--- includes/signup2.inc.php
+++ includes/signup2.inc.php
@@ -19,18 +19,16 @@ if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)
   header("Location: ../index.php?signup=empty");
   exit();
 } else {
-
   //check if input characters are valid
 if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
   header("Location: ../index.php?signup=char");
   exit(); 
 } else {
-
 //check if email is valid
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  header("Location: ../index.php?signup=email");
+  header("Location: ../index.php?signup=email&first=$first&last=$last&uid=$uid");
+  exit();
 } else {
-
   //signup successfully
   $sql = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES (?, ?, ?, ?, ?);";
 
--- index.php
+++ index.php
@@ -10,40 +10,67 @@ include_once 'includes/dbh.inc.php';
   <link type="text/css" rel="stylesheet" href="styles.css">
   <title>PHPdatabase</title>
 </head>
+
 <body>
- 
 <h2>Sign Up</h2>
+
 <form action="includes/signup2.inc.php" method="POST">
-<input type="text" name="first" placeholder="Firstname">
-<br>
-<input type="text" name="last" placeholder="Lastname">
-<br>
+
+<?php
+if (isset($_GET['first'])) {
+  $first =  $_GET['first'];
+  echo '<input type="text" name="first" placeholder="Firstname" value="'.$first.'"><br>';
+}
+else {
+  echo '<input type="text" name="first" placeholder="Firstname"><br>';
+}
+if (isset($_GET['last'])) {
+  $last =  $_GET['last'];
+  echo '<input type="text" name="last" placeholder="Lastname" value="'.$last.'"><br>';
+}
+else {
+  echo '<input type="text" name="last" placeholder="Lastname"><br>';
+}
+?>
 <input type="text" name="email" placeholder="E-mail">
 <br>
-<input type="text" name="uid" placeholder="Username">
-<br>
+<?php
+if (isset($_GET['uid'])) {
+  $uid =  $_GET['uid'];
+  echo '<input type="text" name="uid" placeholder="Username" value="'.$uid.'"><br>';
+}
+else {
+  echo '<input type="text" name="uid" placeholder="Username"><br>';
+}
+?>
 <input type="password" name="pwd" placeholder="Password">
 <br>
 <button type="submit" name="submit">Sign up</button>
 </form>
-<?php
-$fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-if (strpos($fullUrl, "signup=empty") == true) {
+<?php
+if (!isset($_GET['signup'])) {
+exit();
+}
+else {
+$signupCheck = $_GET['signup'];
+
+if ($signupCheck == "empty") {
   echo "<p class='error'>Please fill in all fields!</p>";
   exit();
 }
-elseif (strpos($fullUrl, "signup=char") == true) {
-  echo "<p class='error'>Please use valid characters!</p>";
+elseif ($signupCheck == "char") {
+  echo "<p class='error'>Please make sure to use valid characters for your name!</p>";
   exit();
 }
-elseif (strpos($fullUrl, "signup=email") == true) {
-  echo "<p class='error'>Please use a valid e-mail address!</p>";
+elseif ($signupCheck == "email") {
+  echo "<p class='error'>Please enter a valid e-mail address!</p>";
   exit();
 }
-elseif (strpos($fullUrl, "signup=success") == true) {
+elseif ($signupCheck == "success") {
   echo "<p class='success'>You have been signed up successfully!</p>";
   exit();
+}
 }
 ?>
 </body>
