284b2a04837a942bf09cbb2f51180ddc9ffdc068
mizcaroll <carolomukule@gmail.com>
2020-09-18 11:44:23 +0300

    Remove url file extns & variables with htaccess

--- index.php
+++ index.php
@@ -8,34 +8,6 @@
 </head>
 <body>
 <?php
-//This example is very unique and can be used to generate secure passwords
-
-$conn = mysqli_connect("localhost", "root", "", "phpkeys");
-function checkKeys($conn, $randStr) {
-  $sql = "SELECT * FROM keystring";
-  $result = mysqli_query($conn, $sql);
-  while ($row = mysqli_fetch_assoc($result)) {
-if ($row['keystringKey'] == $randStr) {
-$keyExists = true;
-break;
-} else  {
-  $keyExists = false;
-}
-  }
-  return $keyExists;
-}
-function keyGen($conn) {
-  $KeyLength = 8;
-  $str = "123456789abcdefghijklmnopqrstuvwxyz()/$";
-  $randStr = substr(str_shuffle($str), 0, $KeyLength);
-  $checkKey = checkKeys($conn, $randStr);
-  while ($checkKey == true) {
-    $randStr = substr(str_shuffle($str), 0, $KeyLength);
-    $checkKey = checkKeys($conn, $randStr);
-  }
-  return $randStr;
-}
-echo keyGen($conn);
 
 ?>
 </body>
